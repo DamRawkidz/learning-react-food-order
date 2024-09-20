7885aba4b7a468d68b4493823bddadb07e9338eb
Phet <rawkidz@hotmail.com>
2024-09-21 01:53:18 +0700

    [update] - add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import logoImg from '../assets/logo.jpg'
+export default function header() {
+    return (
+        <header id="main-header">
+            <div id="title">
+                <img src={logoImg} alt="A restaurant" />
+                <h1>ReactFood</h1>
+            </div>
+            <nav>
+                <button>Cart (0)</button>
+            </nav>
+        </header>
+    )
+}
